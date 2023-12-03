9badc65a0daac29775b520850e82b76dd307ae5e
dddnick <140989635+dddnick@users.noreply.github.com>
2023-12-03 11:11:31 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 servos.P0.setAngle(0)
 basic.forever(function () {
-    basic.showNumber(sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1))
+    basic.showNumber(sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0))
     if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1) > 0 && sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1) < 13) {
         servos.P0.setAngle(180)
     } else if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1) == 0 || sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1) > 13) {
